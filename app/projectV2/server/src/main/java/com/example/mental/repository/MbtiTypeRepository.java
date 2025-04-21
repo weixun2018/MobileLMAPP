a1bd33f04a103295e14874e04862b002c9169b66
qwqcoder <qwqcoder@163.com>
2025-04-21 12:33:08 +0800

    在V2应用版本，初步整合Llamacpp模块，实现ai推理问答，初步尝试kv缓存优化，改善多轮对话响应速率

---
+++ app/projectV2/server/src/main/java/com/example/mental/repository/MbtiTypeRepository.java
@@ -0,0 +1,7 @@
+package com.example.mental.repository;
+
+import com.example.mental.entity.MbtiType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MbtiTypeRepository extends JpaRepository<MbtiType, String> {
+} 
